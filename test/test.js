01fac106e02571f15d2313f10472e7694e7ca0cb
Johann Jacobsohn <j.jacobsohn@satzmedia.de>
2013-05-05 09:28:23 +0200

    Add a ontap-helper method, improve readability, add a test

--- test/test.js
+++ test/test.js
@@ -1,11 +1,24 @@
-describe('The App', function () {
+
+//helpers
+enyo.Object.prototype.dotap = function(){ this.bubble("ontap"); }
+
+describe('The app', function () {
+
 	it('has its title set to "Hello World"', function () {
-		expect(app.$.toolbar.content).to.be('Hello World');
+		expect( app.$.toolbar.content ).to.be('Hello World');
 	});
+
 });
-describe('The "Tab me" button', function () {
-	it('adds "The button was tapped." when tabbed', function(){
-		app[app.$.button.ontap]();
-		expect(app.$.main.content).to.be('The button was tapped.<br/>');
+
+describe('The content', function () {
+
+	it('is empty initially', function(){
+		expect( app.$.main.content ).to.be.empty()
 	});
+
+	it('reads "The button was tapped." after the "Tab me" button has been tapped', function(){
+		app.$.button.dotap();
+		expect( app.$.main.content ).to.be('The button was tapped.<br/>');
+	});
+
 });
